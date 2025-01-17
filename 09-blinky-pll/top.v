0361f58764bc392a6b52e24d9ba06726bce2e388
Miodrag Milanovic <mmicko@gmail.com>
2025-01-17 08:57:35 +0100

    clean pll example

--- 09-blinky-pll/top.v
+++ 09-blinky-pll/top.v
@@ -3,20 +3,19 @@ module top (
     output  wire led
 );
 
-    wire clk270, clk180, clk90, clk0, usr_ref_out;
-    wire usr_pll_lock_stdy, usr_pll_lock;
+    wire clk0;
 
 	CC_PLL #(
 		.REF_CLK("10.0"),    // reference input in MHz
-		.OUT_CLK("20"),   // pll output frequency in MHz
+		.OUT_CLK("25"),   // pll output frequency in MHz
 		.PERF_MD("SPEED"), // LOWPOWER, ECONOMY, SPEED
 		.LOW_JITTER(1),      // 0: disable, 1: enable low jitter mode
 		.CI_FILTER_CONST(2), // optional CI filter constant
 		.CP_FILTER_CONST(4)  // optional CP filter constant
 	) pll_inst (
 		.CLK_REF(clk), .CLK_FEEDBACK(1'b0), .USR_CLK_REF(1'b0),
-		.USR_LOCKED_STDY_RST(1'b0), .USR_PLL_LOCKED_STDY(usr_pll_lock_stdy), .USR_PLL_LOCKED(usr_pll_lock),
-		.CLK270(clk270), .CLK180(clk180), .CLK90(clk90), .CLK0(clk0), .CLK_REF_OUT(usr_ref_out)
+		.USR_LOCKED_STDY_RST(1'b0), .USR_PLL_LOCKED_STDY(), .USR_PLL_LOCKED(),
+		.CLK270(), .CLK180(), .CLK90(), .CLK0(clk0), .CLK_REF_OUT()
 	);
 
 
