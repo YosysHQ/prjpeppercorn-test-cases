f22f7598c7c89e8818a92a30ae6edf5be67a57a1
Miodrag Milanovic <mmicko@gmail.com>
2025-05-22 14:57:55 +0200

    Update some more for EVB

--- 022-buttons/top.v
+++ 022-buttons/top.v
@@ -1,6 +1,6 @@
 module top(input rst, input btn0,input btn1,input btn2, input btn3,
        output wire led);
        //assign led = ~(btn0 & btn1 & btn2 &btn3);
-       //assign led = ~(btn0 & btn1);
-       assign led = ~(btn0 & btn1 & btn2 &btn3 & rst);
+       assign led = ~(btn0 & btn1);
+       //assign led = ~(btn0 & btn1 & btn2 &btn3 & rst);
 endmodule
