77411f360d4ef63581c15936d3b027d3d071c866
Miodrag Milanovic <mmicko@gmail.com>
2025-07-01 15:14:09 +0200

    Add few more test cases for eqy

--- check_yosys.py
+++ check_yosys.py
@@ -33,7 +33,7 @@ def run(base_dir, test, freq):
         if len(subdirs)==0:
             raise click.ClickException("Test not found")
     else:
-        allowed = [0,1,2,3,4,5,6,7,8,12,13,19,20,21,22,23,24,25,26,
+        allowed = [0,1,2,3,4,5,6,7,8,9,10,12,13,19,20,21,22,23,24,25,26,
                    27,28,30,31,32,33,34,35,36,37,38,65,66,111]
         filtered_subdirs = []
         for d in subdirs:
