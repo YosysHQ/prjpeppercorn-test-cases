21a16bab8381b6cbcf3e2808f1665af918fb9a39
Miodrag Milanovic <mmicko@gmail.com>
2025-07-09 16:59:20 +0200

    Lut constant inputs

--- verification/000-cc_lut1/top.v
+++ verification/000-cc_lut1/top.v
@@ -1,5 +1,5 @@
-module top(input [3:0] i, output [3:0] o);
-  genvar idx;
+module top(input [3:0] i, output [11:0] o);
+  genvar idx, j;
   generate
     for (idx = 0; idx < 4; idx = idx + 1) begin : gen_lut
       CC_LUT1 #(
@@ -8,6 +8,19 @@ module top(input [3:0] i, output [3:0] o);
         .I0(i[idx]),
         .O(o[idx])
       );
+    end
+  endgenerate
+
+  generate
+    for (idx = 0; idx < 4; idx = idx + 1) begin : gen_lut_const
+      for (j = 0; j < 2; j = j + 1) begin : const_i_input
+        CC_LUT1 #(
+          .INIT(idx[1:0])
+        ) lut_inst_const (
+          .I0(j[0]),
+          .O(o[4 + idx * 2 + j])
+        );
+      end
     end
   endgenerate
 endmodule
