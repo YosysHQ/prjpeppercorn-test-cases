5f64c7337c24e7ba9c629110b3a49a1478e39ea0
Miodrag Milanovic <mmicko@gmail.com>
2025-07-29 10:09:25 +0200

    Add pll without clock buffers

---
+++ 114-bliny-pll-noclkbuf/top.v
@@ -0,0 +1,34 @@
+module top (
+    input  clk,
+    output  wire led
+);
+
+    wire clk0;
+
+	CC_PLL #(
+		.REF_CLK(`BOARD_FREQ_STR),    // reference input in MHz
+		.OUT_CLK("25"),   // pll output frequency in MHz
+		.PERF_MD("SPEED"), // LOWPOWER, ECONOMY, SPEED
+		.LOW_JITTER(1),      // 0: disable, 1: enable low jitter mode
+		.CI_FILTER_CONST(2), // optional CI filter constant
+		.CP_FILTER_CONST(4)  // optional CP filter constant
+	) pll_inst (
+		.CLK_REF(clk), .CLK_FEEDBACK(1'b0), .USR_CLK_REF(1'b0),
+		.USR_LOCKED_STDY_RST(1'b0), .USR_PLL_LOCKED_STDY(), .USR_PLL_LOCKED(),
+		.CLK270(), .CLK180(), .CLK90(), .CLK0(clk0), .CLK_REF_OUT()
+	);
+
+
+    localparam BITS = 8;
+    localparam LOG2DELAY = 21;
+
+    reg [BITS+LOG2DELAY-1:0] counter = 0;
+
+    always @(posedge clk0) begin
+        counter <= counter + 1;
+    end
+
+    assign led = counter[23];
+
+endmodule
+
