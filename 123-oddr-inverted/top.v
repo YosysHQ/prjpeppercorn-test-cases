505657aa6cfda7f7b81f3b56ca35cb2b10af75c9
Miodrag Milanovic <mmicko@gmail.com>
2025-09-22 14:42:47 +0200

    ODDR with inverter

---
+++ 123-oddr-inverted/top.v
@@ -0,0 +1,14 @@
+module top(input wire clk, input wire rst, input wire btn0, input wire btn1, output wire led);
+  wire l;
+  CC_ODDR #(
+    .CLK_INV(1'b0)
+  ) oddr_inst (
+    .D0(btn0),
+    .D1(btn1),
+    .CLK(clk),
+    .DDR(rst),
+    .Q(l)
+  );
+  assign led = ~l;
+
+endmodule
